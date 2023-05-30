43705b4f9ce71875752cf1b86be8f578736346e6
Cuong Dinh <cuongcr1312@gmail.com>
2023-05-30 22:52:43 +0700

    first commit

---
+++ src/main/java/com/journaldev/spring/ConfigSessionFactory.java
@@ -0,0 +1,14 @@
+package com.journaldev.spring;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigSessionFactory {
+//    @Bean
+//    public SessionFactory sessionFactory(SessionFactory sf){
+//        return sf;
+//    }
+
+}
